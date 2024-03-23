e1fb91d45e5ebeb2aa72d89ac0993824c518259c
karthikramx <karthikram570@gmail.com>
2024-03-22 20:43:16 -0700

    blog post grid and styling

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import Card from '../components/Card/Card';
 import { useEffect, useState } from 'react';
+import ReactHtmlParser from 'react-html-parser';
+
 
 export default function Blog() {
     const [items, setItems] = useState([]);
     const [error, setError] = useState(null);
+
+    function extractImageLinks(html) {
+        ReactHtmlParser(html);
+        let imageLinks = [];
+
+        const transform = (node, index) => {
+            if (node.type === 'tag' && node.name === 'img') {
+                imageLinks.push(node.attribs.src);
+            }
+        };
+
+        ReactHtmlParser(html, { transform });
+
+        return imageLinks[0];
+    }
+
+    function extractParagraph(html) {
+        ReactHtmlParser(html);
+        let paragraphs = [];
+
+        const transform = (node, index) => {
+            if (node.type === 'tag' && node.name === 'p') {
+                paragraphs.push(node.children[0].data);
+            }
+        };
+
+        ReactHtmlParser(html, { transform });
+
+        //limit the number of characters to 100
+        if (paragraphs[0] && paragraphs[0].length > 100) {
+            return paragraphs[0].substring(0, 100) + "...";
+        }
+
+
+    }
 
     useEffect(() => {
         async function fetchData() {
@@ -44,20 +81,26 @@ export default function Blog() {
     return (
         <section style={{ marginTop: "100px" }}>
             <div>
-                <h3>Latest Articles</h3>
-                <ul>
-                    {items.map((item, index) => (
-                        <div style={{ justifyContent: 'center', alignItems: 'center' }} key={index}>
+                <h1>My Blogs</h1>
+                <div style={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(auto-fill, minmax(400px, 1fr))',
+                    alignItems: 'center',
+                    justifyItems: 'center',
+                }}>
+                    {items.map((item, index) => {
+                        return (
                             <Card
                                 key={index}
                                 title={item.title}
-                                description={'This will be the description of the article.'}
+                                description={extractParagraph(item.content)}
                                 link={item.link}
-                                image={'https://cdn-images-1.medium.com/v2/resize:fit:1024/0*5qM8xcEbuRwAV1LO'}
-                            />
-                        </div>
-                    ))}
-                </ul>
+                                image={extractImageLinks(item.content)}
+                            />)
+
+
+                    })}
+                </div>
                 <a
                     href={"https://karthikramx.medium.com/"}
                     target={"_blank"}
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,15 +3,15 @@ import React from 'react';
 import './Card.css'; // Import the CSS
 
 const Card = ({ image, title, description, link }) => {
-
     return (
-        <a href={link} target="_blank" rel="noopener noreferrer">
-            <div className="card">
-                <img src={image} alt={title} />
-                <h4>{title}</h4>
-                <p>{description}</p>
-            </div>
-        </a>
+        <div className="card" style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <img src={image} alt={title} />
+            <h4>{title}</h4>
+            <p>{description}</p>
+            <a href={link} target="_blank" rel="noopener noreferrer">Read More</a>
+        </div>
+
+
     );
 }
 
